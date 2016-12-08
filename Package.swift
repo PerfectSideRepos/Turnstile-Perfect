4d9289012223fc4ebab5a84a4b77ad263c3f96b7
Jonathan Guthrie <jono@perfect.org>
2016-12-08 10:34:58 -0500

    adding swift string and mustache

--- Package.swift
+++ Package.swift
@@ -1,9 +1,13 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-12-08 15:32:40 +0000
 import PackageDescription
-
 let package = Package(
     name: "TurnstilePerfect",
-  dependencies: [
-    .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
-    .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1)
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
     ]
 )
