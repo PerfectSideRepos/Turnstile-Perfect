19b2de643143dce015b388d1adf745b3196c1d0b
Jonathan Guthrie <jono@perfect.org>
2017-01-05 10:59:15 -0500

    Updating http server version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 15:32:40 +0000
+// Date: 2017-01-05 15:58:35 +0000
 import PackageDescription
 let package = Package(
     name: "TurnstilePerfect",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 1),
         .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
