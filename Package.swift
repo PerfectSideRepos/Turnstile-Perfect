844b5f76e3ff5e413674b9e1c440d800f95f6cc9
Jonathan Guthrie <jono@guthrie.net.nz>
2017-04-03 13:31:43 -0400

    update minors

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ let package = Package(
     name: "TurnstilePerfect",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 1),
-        .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
+        .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1),
         .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
     ]
 )
