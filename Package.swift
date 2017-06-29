d39dac141d9692d8e4d12d62e4335cd1d9bad963
Jonathan Guthrie <jono@perfect.org>
2017-06-29 16:33:58 -0400

    update to v2 SwiftString

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
         .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
     ]
 )
