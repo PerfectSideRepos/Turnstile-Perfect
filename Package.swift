c40fcb79a2f103b93f044b0f682e4abb27e77cec
Jonathan Guthrie <jono@perfect.org>
2017-10-04 17:44:11 -0400

    update to latest

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ let package = Package(
     name: "TurnstilePerfect",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
         .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
         .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
     ]
 )
