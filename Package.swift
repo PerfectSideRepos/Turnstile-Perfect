e8bf9e3af58ed68312d8531be65397fda268f8ac
Jonathan Guthrie <jono@perfect.org>
2017-10-11 16:10:00 -0400

    cosmetic change

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-05 15:58:35 +0000
+// Date: 2017-10-11 20:04:46 +0000
 import PackageDescription
 let package = Package(
-    name: "TurnstilePerfect",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
-        .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
-    ]
+	name: "TurnstilePerfect",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+		.Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
+	]
 )
